60563661d0698f639e693a5ee073a9ca614e8639
Cameron Henlin <cameron@ctl-mac-074.local>
2014-03-20 10:59:36 -0700

    added forums view

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ var ThreadTileView = Backbone.View.extend({
     },
 	template: _.template('\
 		<div class="live-tile " <%= colors[Math.floor((Math.random()*6))] %> " data-speed="1750" \
-		data-delay="<%= Math.floor((Math.random()*5000)+2000) %>" onclick="handleLink(<%= id %>);"> \
+		data-delay="<%= Math.floor((Math.random()*5000)+2000) %>"> \
 			<span class="tile-title"><%= replies %> replies<br> ruined by <%= killedBy %></span> \
 			<div style="font-size: 15px;"><%= name %></div> \
 			<div style="font-size: 25px;"> started by <%= author %></div> \
@@ -63,15 +63,81 @@ var ThreadTileView = Backbone.View.extend({
 	}
 });
 
+var ForumTileModel = Backbone.Model.extend({
+	defaults: function () {
+		return {
+			"id"          : "",
+			"name"        : "",
+			"description" : "",
+			"moderators"  : ""
+		}
+	}
+});
+
+var ForumTilesView = Backbone.View.extend({
+	template: _.template('<div class="tiles blue tile-group four-wide"></div>'),
+	initialize: function() {
+		this.setElement(this.template());
+		_.bindAll(this, 'render', 'renderOne');
+		if (this.model) {
+			this.model.on('change', this.render, this);
+		}
+	},
+	render: function() {
+		this.collection.each(this.renderOne);
+		return this;
+	},
+	renderOne: function(model) {
+		console.log(model);
+		var forumTile = new ForumTileView({ model: model });
+		this.$el.append(forumTile.render().$el);
+		return this;
+	}
+});
+
+var ForumTileView = Backbone.View.extend({
+	model: ForumTileModel,
+	events: {
+        "click": function() {
+			$('body').html("");
+        	sRA.getThreads(this.model.get("id"), function(threads) {
+				var threadTileCollection = new Backbone.Collection();
+				threads.forEach(function(thread) {
+					threadTileCollection.add(new ThreadTileModel(thread));
+				});
+				var threadTilesView = new ThreadTilesView({collection: threadTileCollection});
+		   		$("body").append( threadTilesView.render().$el );
+
+				$(".live-tile, .flip-list").not(".exclude").liveTile();
+			});
+       	}
+    },
+	template: _.template('\
+		<div class="live-tile " <%= colors[Math.floor((Math.random()*6))] %> " data-speed="1750" \
+		data-delay="<%= Math.floor((Math.random()*5000)+2000) %>"> \
+			<span class="tile-title"><%= description %></span> \
+			<div style="font-size: 15px;"><%= name %></div> \
+			<div style="font-size: 25px;"> moderated by <%= moderators %></div> \
+		</div> \
+    '),
+	initialize: function() {
+		this.model.bind("change", this.render, this);
+	},
+	render: function() {
+		this.setElement(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
 $('doccument').ready(function() {
 	$('body').html("");
-	sRA.getThreads(219, function(threads) {
-		var threadTileCollection = new Backbone.Collection();
-		threads.forEach(function(thread) {
-			threadTileCollection.add(new ThreadTileModel(thread));
+	sRA.getForums(function(forums) {
+		var forumsTileCollection = new Backbone.Collection();
+		forums.forEach(function(forums) {
+			forumsTileCollection.add(new ForumTileModel(forums));
 		});
-		var threadTilesView = new ThreadTilesView({collection: threadTileCollection});
-   		$("body").append( threadTilesView.render().$el );
+		var forumTilesView = new ForumTilesView({collection: forumsTileCollection});
+   		$("body").append( forumTilesView.render().$el );
 
 		$(".live-tile, .flip-list").not(".exclude").liveTile();
 	});
